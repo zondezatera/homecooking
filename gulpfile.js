8a2f891b9e91ad3c44df399878fb6485a74a1dcd
Kitti Pariyaakkarakun <zatera1920@gmail.com>
2015-07-25 04:01:11 +0700

    Set Env and Structure

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+var jsonServer = require('gulp-json-srv');
+
+gulp.task('database-connect',function() {
+	jsonServer.start({
+      data: 'db.json',
+      port: 3123
+    });
+});
+gulp.task('web-connect', function() {
+  connect.server({
+  	root:'resources',
+    port: 8888
+  });
+});
+
+gulp.task('default', ['database-connect','web-connect']);
